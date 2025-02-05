daab26f351d6f27186da3f14df231275af8b6e47
atamas <tamas.andor@yahoo.com>
2025-02-05 18:40:46 +0100

    Improve readability

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:22:53 by atamas            #+#    #+#             */
-/*   Updated: 2025/01/03 17:33:18 by atamas           ###   ########.fr       */
+/*   Updated: 2025/02/05 18:39:13 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,20 @@
 int	main()
 {
 	const Animal* meta = new Animal();
-	const Animal* i = new Cat();
-	const Animal* j = new Dog();
+	const Animal* cat = new Cat();
+	const Animal* dog = new Dog();
 	const WrongAnimal* wAnimal = new WrongAnimal();
 	const WrongAnimal* cAnimal = new WrongCat();
 
-	i->makeSound();
-	j->makeSound();
+	cat->makeSound();
+	dog->makeSound();
 	meta->makeSound();
-	std::cout << "<<Wrong test start from here>>\n\n";
+	std::cout << "\n<<Wrong test start from here>>\n\n";
 	wAnimal->makeSound();
 	cAnimal->makeSound();
 	delete meta;
-	delete i;
-	delete j;
+	delete cat;
+	delete dog;
 	delete wAnimal;
 	delete cAnimal;
 	return (0);
